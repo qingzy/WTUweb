945a52fb95e0b4a3f362bee65bd2bc84c270c08b
qingzy <1654037035@qq.com>
2020-10-21 14:32:47 +0800

    第三次web作业

---
+++ login3/login/src/login/dao/UserDao.java
@@ -0,0 +1,20 @@
+package login.dao;
+
+
+import login.domain.Users;
+
+
+
+public interface UserDao {
+
+		//查询单个用户信息
+		public Users queryUser(String userName,String password);
+
+		public int queryUsername(String userName);
+
+		public int queryUserPwd(String userName, String password);
+
+		public int addUsers(Users users);
+		
+	
+}
